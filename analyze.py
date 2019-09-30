e52f6a0167b93a6a1f0effddfc26bb6c6cf8e8fe
Fabián Villena <villenafabian@gmail.com>
2019-09-30 14:17:19 +0200

    sentiment analysis

--- analyze.py
+++ analyze.py
@@ -1,8 +1,11 @@
 from src.data.survey_data import SurveysLoader
 from src.data.text_data import Preprocessor
+from src.analysis.sentiment import SentimentAnalyzer
 loader = SurveysLoader('data/raw/')
 loader.write_surveys('data/interim/surveys.json')
 preprocessor = Preprocessor('data/interim/surveys.json')
 preprocessor.preprocess()
 preprocessor.write_surveys('data/interim/surveys_preprocessed.json')
-
+analyzer = SentimentAnalyzer('data/interim/surveys.json','data/interim/surveys_preprocessed.json','data/external/Ratings_Warriner_et_al_Spanish.csv')
+analyzer.analyze()
+analyzer.write_result('results/surveys_analyzed.csv')
